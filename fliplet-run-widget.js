ab9db355ec844ad890714cbd9928231c5fdca0ff
Nicholas Valbusa <squallstar@gmail.com>
2016-06-29 13:44:15 +0200

    support for dependencies

--- fliplet-run-widget.js
+++ fliplet-run-widget.js
@@ -6,6 +6,8 @@ const path = require('path');
 const folderPath = process.cwd();
 const packagePath = path.join(folderPath, 'widget.json');
 
+const assets = require(path.join(__dirname, 'lib', 'assets'));
+
 var app = express();
 
 var package;
@@ -30,6 +32,26 @@ app.use(express.static(folderPath, { maxage: '1h' }));
 const runWidgetHtml = Handlebars.compile(fs.readFileSync(path.join(__dirname, 'assets', 'run-widget.html'), 'utf8'));
 app.get('/', function (req, res) {
   res.send(runWidgetHtml(package));
+});
+
+app.get('/build', function (req, res) {
+  fs.readFile('./build.html', 'utf8', function (err, data) {
+    if (!data) {
+      return res.send('The build.html file was not found');
+    }
+
+    res.send(assets.html(data, assets.parse(package.build.dependencies)));
+  });
+});
+
+app.get('/interface', function (req, res) {
+  fs.readFile('./interface.html', 'utf8', function (err, data) {
+    if (!data) {
+      return res.send('The interface.html file was not found');
+    }
+
+    res.send(assets.html(data, assets.parse(package.interface.dependencies)));
+  });
 });
 
 // --------------------------------------------------------------------------
