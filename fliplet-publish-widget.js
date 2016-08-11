234ce004e843895d19699463f9ac70bede815015
Ricardo Seromenho <ricardo@seromenho.com>
2016-08-11 15:09:59 +0100

    Fix: sending package zip file

--- fliplet-publish-widget.js
+++ fliplet-publish-widget.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const archiver = require('archiver');
 const temp = require('temp');
 
+console.log('Publishing...');
+
 var email = configstore.get('email');
 var auth_token = configstore.get('auth_token');
 
@@ -17,47 +19,46 @@ var tempName = temp.path({suffix: '.zip'});
 var output = fs.createWriteStream(tempName);
 var archive = archiver('zip');
 
-output.on('close', function() {
-  console.log('Widget package zipped...');
-});
-
 archive.on('error', function(err) {
   throw err;
 });
 
-console.log('Creating zip package...');
+archive.on('end', function(err) {
+  var zip = fs.createReadStream(tempName);
+
+  request({
+    method: 'POST',
+    url: config.api_url + 'v1/widgets?auth_token=' + auth_token,
+    formData: {
+      my_file: zip
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+
+    body = JSON.parse(body);
+
+    if (response.statusCode !== 200) {
+      console.log(body);
+    } else {
+      console.log('The widget ' + body.widget.name + '(' + body.widget.version + ') has been imported');
+    }
+
+    // Remove temporary file
+    fs.unlinkSync(tempName);
+  });
+});
+
+archive.pipe(output);
+
 archive
   .bulk([
-  {
-    cwd: './',
-    src: ['**/*', '!.git/**', '!.gitignore'],
-    expand: true,
-    dest: ''
-  }
-])
-  .pipe(output);
-archive.finalize();
-
-console.log('Publishing...');
-request({
-  method: 'POST',
-  url: config.api_url + 'v1/widgets?auth_token=' + auth_token,
-  formData: {
-    my_file: fs.createReadStream(tempName)
-  }
-}, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-
-  body = JSON.parse(body);
-
-  if (response.statusCode !== 200) {
-    console.log(body);
-  } else {
-    console.log('The widget ' + body.widget.name + '(' + body.widget.version + ') has been imported');
-  }
-
-  // Remove temporary file
-  fs.unlinkSync(tempName);
-});
+    {
+      cwd: './',
+      src: ['**/*', '!.git/**', '!.gitignore'],
+      expand: true,
+      dest: ''
+    }
+  ])
+  .finalize();
