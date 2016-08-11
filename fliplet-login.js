ad313b380cc431b7733478b6b0e0c8ecd457ba93
Ricardo Seromenho <ricardo@seromenho.com>
2016-08-11 15:09:42 +0100

    Login improvements

--- fliplet-login.js
+++ fliplet-login.js
@@ -3,12 +3,21 @@ const prompt = require('prompt');
 
 console.log('Login...');
 prompt.start();
-prompt.get(['email', 'password'], function (err, result) {
+prompt.get([
+  'email',
+  {
+    name: 'password',
+    hidden: true }
+], function (err, result) {
   auth.login({
     email: result.email,
     password: result.password
   })
     .then(function(login) {
       console.log('Logged in');
+      console.log('Auth-token: ', login.auth_token);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 });
