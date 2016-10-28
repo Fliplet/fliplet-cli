2d7dd59d94d91b94604361cdff9627e879052c4f
Ricardo Seromenho <ricardo@seromenho.com>
2016-10-28 13:17:20 +0100

    List organizations command

---
+++ fliplet-list-organizations.js
@@ -0,0 +1,19 @@
+const organizations = require('./lib/organizations');
+
+log('Requesting up to date organizations list from the server...');
+
+organizations.getOrganizationsList()
+  .then(function onGetOrganizations(organizations) {
+    log('');
+    organizations.forEach(function (organization) {
+      log(`• ${organization.id}: ${organization.name}`);
+      log('');
+    });
+  })
+  .catch(function onGetOrganizationsError(error) {
+    log(error);
+  });
+
+function log() {
+  console.log.apply(this, arguments);
+}
--- fliplet.js
+++ fliplet.js
@@ -10,6 +10,7 @@ program
   .command('run-widget', 'Run the current widget for development')
   .command('publish-widget', 'Publish the current widget on fliplet studio')
   .command('list-assets', 'Gets the list of the available assets in the system')
+  .command('list-organizations', 'Gets the list of the available organizations in the system')
   .command('env [name]', 'Set the environment: dev, staging or production')
   .command('login', 'Log in with your Fliplet Studio account')
   .command('logout', 'Log out from Fliplet Studio')
