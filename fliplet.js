7aeb9e73613fb7ec02c83639eef3d5e4f19ff798
Nicholas Valbusa <squallstar@gmail.com>
2017-02-01 12:22:33 +0100

    support for themes development

--- fliplet.js
+++ fliplet.js
@@ -6,13 +6,14 @@ const program = require('commander');
 
 program
   .version(package.version)
-  .command('create-widget [name]', 'Create a new widget')
-  .command('run-widget', 'Run the current widget for development')
-  .command('publish-widget', 'Publish the current widget on fliplet studio')
-  .command('list-assets', 'Gets the list of the available assets in the system')
-  .command('list-organizations', 'Gets the list of the available organizations in the system')
+  .command('create-widget [name]', 'Create a new widget.')
+  .command('create-theme [name]', 'Create a new theme.')
+  .command('run', 'Run the current widget or theme for development.')
+  .command('publish', 'Publish the current widget or theme on fliplet studio.')
+  .command('list-assets', 'Gets the list of the available assets in the system.')
+  .command('list-organizations', 'Gets the list of the available organizations in the system.')
   .command('organization [id]', 'Set current working organization. Use without id to reset.')
-  .command('env [name]', 'Set the environment: dev, staging or production')
-  .command('login', 'Log in with your Fliplet Studio account')
-  .command('logout', 'Log out from Fliplet Studio')
+  .command('env [name]', 'Set the environment: dev, staging or production.')
+  .command('login', 'Log in with your Fliplet Studio account.')
+  .command('logout', 'Log out from Fliplet Studio.')
   .parse(process.argv);
