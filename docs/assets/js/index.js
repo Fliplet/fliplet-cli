c23cdeba5415e4fd7fa1bdf6f43b75510e084c77
Nicholas Valbusa <squallstar@gmail.com>
2018-04-04 18:27:50 +0100

    search results

---
+++ docs/assets/js/index.js
@@ -0,0 +1,35 @@
+var search = instantsearch({
+  appId: '8GRBFEV21Y',
+  apiKey: '4c075de739ed6724ede1f923f3d42abf',
+  indexName: 'docs',
+  urlSync: false,
+  searchFunction: function (helper) {
+    if (helper.state.query === '') {
+      return;
+    }
+
+    helper.search();
+  }
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search',
+    placeholder: 'Type to search'
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: [
+        '<a href="{{{url}}}#{{{anchor}}}"><h4>{{{_highlightResult.title.value}}}</h4>',
+        '<p>{{{_highlightResult.content.value}}}</p><hr /><span>{{{headings}}} <strong>({{{anchor}}})</strong></span></a>'
+      ].join(''),
+      empty: 'We didn\'t find any result under our documentation for your query.'
+    }
+  })
+);
+
+search.start();
