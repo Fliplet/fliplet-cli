5ad8f9ed87c7530234a47bdbda6edd61e54ed646
Nicholas Valbusa <squallstar@gmail.com>
2022-01-26 17:26:42 +0100

    tweaks

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -11,12 +11,13 @@ window.algoliaSearch = docsearch({
   }
 });
 
-$('input.ds-input').on('keyup', function (e) {
+$('input.ds-input').on('keyup', function(e) {
   var value = $(this).val();
+
   $(this).closest('.search-input').toggleClass('is-searching', !!value);
 });
 
-$('[data-clear-search]').click(function (e) {
+$('[data-clear-search]').click(function(e) {
   e.preventDefault();
   algoliaSearch.input.autocomplete.setVal('');
   algoliaSearch.input.autocomplete.close('');
@@ -52,8 +53,9 @@ $(window).on('scroll', onScroll);
 onScroll();
 
 // Accordion logic
-$('body').on('click', 'a.toggle', function (event) {
+$('body').on('click', 'a.toggle', function(event) {
   event.preventDefault();
+
   var $li = $(this).parent();
   var isOpen = $li.hasClass('active');
 
@@ -67,7 +69,7 @@ $('body').on('click', 'a.toggle', function (event) {
 });
 
 // Tweaks for algolia search
-$('body').on('blur', '.ais-search-box--input', function () {
+$('body').on('blur', '.ais-search-box--input', function() {
   $(this).removeClass('in-focus');
 });
 
@@ -87,35 +89,37 @@ if (location.pathname !== '/') {
   var added;
 
   // Generate Table of Contents
-  $('.main-content h2, .main-content h3').each(function () {
+  $('.main-content h2, .main-content h3').each(function() {
     added = true;
 
     $title = $(this);
+
     var node = $title[0].tagName;
     var prefix = node === 'H4' ? '- ' : '';
     var text = $title.text();
 
     var $el = $('<a href="#" data-to-id="' + $title[0].id + '" class="title-' + node + '">' + prefix + text + '</a>');
+
     $tocList.append($el);
 
-    $el.click(function (e) {
+    $el.click(function(e) {
       e.preventDefault();
 
       var targetId = $(this).attr('data-to-id');
 
       history.pushState(null, text, location.pathname + '#' + targetId);
 
-      var scrollOffset = $('#' + targetId)[0].offsetTop - $("#page-content")[0].offsetTop;
+      var scrollOffset = $('#' + targetId)[0].offsetTop - $('#page-content')[0].offsetTop;
 
-      $("body, html").animate({
+      $('body, html').animate({
         scrollTop: scrollOffset - 30
-        }, 500);
+      }, 500);
 
       var $target = $('#' + targetId);
 
       $target.css('background-color', '#fdff6b').addClass('animated shake');
 
-      setTimeout(function () {
+      setTimeout(function() {
         $target.css('background-color', 'transparent');
       }, 5000);
     });
@@ -129,6 +133,7 @@ if (location.pathname !== '/') {
   $html.removeClass('with-sidebar');
   $html.addClass('with-hero');
 
+  $('.hiring').slideDown(300);
   $('.project-name').remove();
 
   new Typed('.hero h6', {
@@ -165,7 +170,7 @@ if (location.pathname.indexOf('/API/helpers/') === 0) {
 }
 
 // Menu on mobile
-$('.menu-handle').click(function (e) {
+$('.menu-handle').click(function(e) {
   e.preventDefault();
   $('html').toggleClass('with-mobile-menu');
 });
